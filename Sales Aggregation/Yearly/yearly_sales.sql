3426cfd5b4dee388466973eb885c7ab4ae88862a
Adham Mostafa <adhammostafa1121@gmail.com>
2025-12-11 12:01:04 +0200

    Add files via upload

---
+++ Sales Aggregation/Yearly/yearly_sales.sql
@@ -0,0 +1,12 @@
+-- yearly_sales.sql
+-- Yearly sales metrics
+
+SELECT 
+    YEAR(order_date) AS order_year,
+    SUM(sales_amount) AS total_year_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date)
+ORDER BY order_year ASC;
